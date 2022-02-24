e408d4847067143cc54e1b9e561056b482ec73ee
JessicaSanto <jessicaesanto@outlook.com>
2022-02-24 14:54:12 -0300

    angular1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatComponent } from './view/contat/contat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "contato", component: ContatComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContatComponent } from './view/contat/contat.component';
+import { JogosComponent } from './view/jogos/jogos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatComponent,
+    JogosComponent
   ],
   imports: [
     BrowserModule,
